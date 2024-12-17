9c9e9f6fa6d9b622108e38754f88116a542dc0e0
itachialy <zassuo@gmail.com>
2024-12-17 16:38:32 +0700

    update giao diện trang chủ

--- src/main/java/com/c0324/casestudym5/Casestudym5Application.java
+++ src/main/java/com/c0324/casestudym5/Casestudym5Application.java
@@ -2,8 +2,9 @@ package com.c0324.casestudym5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Casestudym5Application {
 
     public static void main(String[] args) {
