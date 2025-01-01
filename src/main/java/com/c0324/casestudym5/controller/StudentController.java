f213c2fc713d219fa6d20aaf8fd42caad9e52f31
Cong dep trai <phamthanhcong0709@gmail.com>
2025-01-01 13:09:09 +0700

    update register topic

--- src/main/java/com/c0324/casestudym5/controller/StudentController.java
+++ src/main/java/com/c0324/casestudym5/controller/StudentController.java
@@ -2,15 +2,21 @@ package com.c0324.casestudym5.controller;
 
 import com.c0324.casestudym5.dto.RegisterTopicDTO;
 import com.c0324.casestudym5.model.Student;
+import com.c0324.casestudym5.model.Topic;
 import com.c0324.casestudym5.service.StudentService;
 import com.c0324.casestudym5.service.TeamService;
 import com.c0324.casestudym5.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/student")
@@ -36,13 +42,19 @@ public class StudentController {
     }
 
     @GetMapping("/register-topic")
-    public String showRegistTopicForm(Model model) {
+    public String showRegisterTopicForm(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Topic> topicPage = topicService.getAllTopics(pageable);
         model.addAttribute("registerTopic", new RegisterTopicDTO());
+        model.addAttribute("topics", topicPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", topicPage.getTotalPages());
         return "student/register-topic";
     }
 
     @PostMapping("/register-topic")
-    public String registTopic(@ModelAttribute RegisterTopicDTO registerTopicDTO, Principal principal) {
+    public String registerTopic(@ModelAttribute RegisterTopicDTO registerTopicDTO, Principal principal) {
         topicService.registerTopic(registerTopicDTO, principal.getName());
         return "redirect:/student/team";
     }
--- src/main/java/com/c0324/casestudym5/service/TopicService.java
+++ src/main/java/com/c0324/casestudym5/service/TopicService.java
@@ -1,8 +1,13 @@
 package com.c0324.casestudym5.service;
 
 import com.c0324.casestudym5.dto.RegisterTopicDTO;
+import com.c0324.casestudym5.model.Topic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface TopicService {
 
     void registerTopic(RegisterTopicDTO registerTopicDTO, String username);
+
+    Page<Topic> getAllTopics(Pageable pageable);
 }
