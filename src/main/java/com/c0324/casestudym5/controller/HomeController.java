363b2aab5737e3d12cb08b5ff376efba807f2c2c
Itachialy <zassuo@gmail.com>
2025-01-23 07:30:23 +0700

    merge huycommit

--- src/main/java/com/c0324/casestudym5/controller/HomeController.java
+++ src/main/java/com/c0324/casestudym5/controller/HomeController.java
@@ -6,7 +6,9 @@ import com.c0324.casestudym5.service.BlogsService;
 import com.c0324.casestudym5.service.NotificationService;
 import com.c0324.casestudym5.service.TopicService;
 import com.c0324.casestudym5.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,15 +33,11 @@ public class HomeController {
     }
 
     @GetMapping(value = {"/", "/home"})
-    public String homePage(Model model, Principal principal) {
-        User currentUser = userService.findByEmail(principal.getName());
-
-        List<NotificationDTO> notifications = notificationService.getTop3NotificationsByUserIdDesc(currentUser.getId());
+    public String homePage(Model model ) {
         List<Topic> latestTopics = topicService.getLatestTopics(3);
         List<Blogs> latestBlogs = blogsService.getLatestBlogs(5);
 
         model.addAttribute("topics", latestTopics);
-        model.addAttribute("notifications", notifications);
         model.addAttribute("blogs", latestBlogs);
 
         return "common/home-page";
