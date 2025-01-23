96116bf0673145417d6bf1d3670735f14adb6de4
NgocLinhBuii <ngoclinhh29@gmail.com>
2025-01-23 22:57:33 +0700

    update

--- src/main/java/com/c0324/casestudym5/dto/TeacherSearchDTO.java
+++
@@ -1,8 +0,0 @@
-package com.c0324.casestudym5.dto;
-import lombok.Data;
-@Data
-public class TeacherSearchDTO {
-    private String name = null;
-    private String email = null;
-    private String phoneNumber = null;
-}
