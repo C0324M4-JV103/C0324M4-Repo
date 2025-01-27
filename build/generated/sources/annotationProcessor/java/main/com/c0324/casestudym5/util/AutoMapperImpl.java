4b0a5e925b23dd41db66b1ab8b4405f4d79ecca0
Itachialy <zassuo@gmail.com>
2025-01-27 09:41:08 +0700

    fix layout navbar + logic register team

---
+++ build/generated/sources/annotationProcessor/java/main/com/c0324/casestudym5/util/AutoMapperImpl.java
@@ -0,0 +1,11 @@
+package com.c0324.casestudym5.util;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-25T09:44:19+0700",
+    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.11.1.jar, environment: Java 17.0.13 (Amazon.com Inc.)"
+)
+public class AutoMapperImpl implements AutoMapper {
+}
