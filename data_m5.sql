90c8c64bc82061a92a7156c0f27b3506992f7b9d
Itachialy <zassuo@gmail.com>
2025-02-06 23:11:56 +0700

    up

---
+++ data_m5.sql
@@ -0,0 +1,144 @@
+INSERT INTO clazz (name) VALUES
+('C0324'),
+('C0527'),
+('C0728'),
+('C0930');
+
+INSERT INTO faculty (name)
+VALUES
+    ('Khoa Công nghệ thông tin'),
+    ('Khoa Công nghệ phần mềm'),
+    ('Khoa Mạng máy tính'),
+    ('Khoa Khoa học dữ liệu'),
+    ('Khoa An toàn thông tin');
+INSERT INTO role (name)
+VALUES
+    ('ROLE_STUDENT'),
+    ('ROLE_TEACHER');
+
+
+INSERT INTO multi_file (url) VALUES
+-- 10 giảng viên
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/83522681_179735500044067_3542104136541536256_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=-UXhDZJjJs4Q7kNvgG7VDeE&_nc_oc=AdistM39CyLrQlHDHscndnmG7NP92MgwUp-Wa8MV08E9wqzpO-7NHnpq1B5c44ymucWCcbvAJ3D4WIeEBZ605Es3&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=AdGqXgDorzy86hbQyuJyzEx&oh=00_AYCLDTP1q0XPtdBHFO9YOh5z-1AMIr94seUVfzDBfA3QxQ&oe=67C6AF32'),
+('https://scontent.fdad2-1.fna.fbcdn.net/v/t1.6435-9/84547019_179735530044064_1742795414005874688_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=ufYi7K3XmG0Q7kNvgG4yHeM&_nc_oc=Adh-bAxC3U8nAhoVObCtSFzzrW8wv19O8aJB-Kr4xbJRjeDX8KoZoYHp83RkDvljCvqnZmS6tJYc0PpgjKgV55ci&_nc_zt=23&_nc_ht=scontent.fdad2-1.fna&_nc_gid=A_sgpX2ZHLbbu6y7XIes0JC&oh=00_AYA8KNCJfUgUw0cmOL1Bo_L3SeZJmBxFr4TiEe_K0N8WOA&oe=67C6D97D'),
+('https://scontent.fdad1-4.fna.fbcdn.net/v/t1.6435-9/83384378_179735570044060_4898624472711430144_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=rQAQuhChRkEQ7kNvgHc6TlG&_nc_oc=Adi8uDAPCdvvc_Gz5sNP4yLbzM2FtEup6XfbV1Fne6qGu1Vv8S5R8wmtMW9zQvfl-wQ5JpRa6mXRq2_qt63n6o_R&_nc_zt=23&_nc_ht=scontent.fdad1-4.fna&_nc_gid=AnA75oKw-DsUW08HWNcEEpw&oh=00_AYChne510q5KTVjq52UauC7HE2tKZvCXCHrx2GtPAe9jJQ&oe=67C6E0C5'),
+('https://bloganchoi.com/wp-content/uploads/2020/08/hua-quang-han-0.jpg'),
+('https://cdn.codegym.vn/wp-content/uploads/2023/01/27.Nguyen-Thanh-Cong-_-Giang-Vien.JPG.jpg'),
+('https://vnn-imgs-a1.vgcloud.vn/icdn.dantri.com.vn/2021/05/26/ngo-ngang-voi-ve-dep-cua-hot-girl-anh-the-chua-tron-18-docx-1622043349706.jpeg'),
+('https://toigingiuvedep.vn/wp-content/uploads/2021/07/mau-anh-the-dep-chat-luong.jpg'),
+('https://mikiways.com/wp-content/uploads/2024/03/abc3da528d36a9a3f06e8682fd534c38.jpg'),
+('https://ss-images.saostar.vn/wp700/pc/1617160792140/saostar-i7akgwmdp6crid24.jpg'),
+('https://mikiways.com/wp-content/uploads/2024/03/abc3da528d36a9a3f06e8682fd534c38.jpg'),
+-- 20 sinh viên
+('https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/448882100_998304525003027_67832813577342325_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=utpreha9RecQ7kNvgHVTIw1&_nc_oc=AdjtelvVzrBD1brT25N6iSxPhVyick04nyiT1l9Co-6S85dk4cUw0v2VVdQPWqwff6qyKTwIXmVVAcXGd7cckqkA&_nc_zt=23&_nc_ht=scontent.fdad1-3.fna&_nc_gid=Apy9nMCAZl1Q_qaFtbEFlPO&oh=00_AYCS-sqz7faKF7_fzAlXa89Rt02XL_HOterhKWsH80kDFw&oe=67A41B42'),
+('https://scontent.fdad2-1.fna.fbcdn.net/v/t39.30808-6/448998604_998304725003007_4872423917308754206_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=wkcZRbv2myYQ7kNvgFld3iO&_nc_oc=Adi6OksatvvUSc5SdnXIpt9QMZzMmJyvtJjj8ZIZdigdYi1J5jg8jeMrL6V_z-5U30kUJzblGr7T9xgG4v2MkNuJ&_nc_zt=23&_nc_ht=scontent.fdad2-1.fna&_nc_gid=AWUXbIkYWGCddPdn5AVS0Jv&oh=00_AYCj_KiAvhtBNwSyG2_Pn4RJTJwTDtBu3JcXlQbLTCCYeQ&oe=67A42A9A'),
+('https://scontent.fdad2-1.fna.fbcdn.net/v/t39.30808-6/449212512_998304608336352_1017940927835428270_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=MDEowqReQdEQ7kNvgFG9Y8E&_nc_oc=AdgjYOqesSTSCEWRKXic5gDpL9pwanbLh85nnRcLj4ibv24MNviB3DNBZdvMGB9yW8uwjKa8uItMw0n39vsH7PZp&_nc_zt=23&_nc_ht=scontent.fdad2-1.fna&_nc_gid=A8cogBhyQi3t0TdAWDTY6yk&oh=00_AYAl1-6rwPtfaASkG1a3vDoo7jOKpJ0_7tXvTk8-ZXoiXw&oe=67A3FE0E'),
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/449361416_998304771669669_4917415649011583224_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=FzEvrAg1Q-kQ7kNvgHWmfa2&_nc_oc=AdiTXLHy1YFZuYbRDtvn726i-n1p5kbSzf5DdZ_5iFFpseZ91I0O0-T1o1fIoYxwwa-08WqSrgIajBGddM9dwUJj&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=A7CAIViK5J4KsJ_qS0bb1ed&oh=00_AYCgB_7DP3RewBD26tzESh7RB4D7sro38sb41rVMoRw-Cw&oe=67A420AF'),
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/473963468_1874382380008110_5067631302134118382_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=dm3slUusgKoQ7kNvgGeiHZv&_nc_oc=Adj0rtwupjdIVVSc-cT2H17k5exodB7IUhNJ8SdH1nKmVzYMZ1caS0thyD-AiQdk8e8h_L7roFRk65Jvludk1W5K&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=AoWqVr_YyanRKyZ6uUojHmZ&oh=00_AYDLLCTGOX0F3uv1B1yNv0lySM61HSd9j5c6u3xXS3tT0Q&oe=67A41A2D'),
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/83391460_873609399727450_1712206991931211776_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=YsQE40Zx1q4Q7kNvgHJ_FFm&_nc_oc=Adhi7RKKONJQOKNwHJHARCSaiJP4Ta4Ewgm162B3F1vvY5D-IQa46QHiqRVu2EY4-wmtNlar4yfxBGjQVAKHCxvs&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=AIMswSic4Ib8NFrJ0_Vgvry&oh=00_AYD_W-2lsKo42Cxynt5Mzk4nnKbfoaw_MOZqMpxmzuofJg&oe=67C59E7B'),
+('https://scontent.fdad1-3.fna.fbcdn.net/v/t1.6435-9/83967962_873609403060783_1818762124067864576_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=7FAwuWFpSn0Q7kNvgGRvCQv&_nc_oc=AdhU9Lqw0r8LIv3KlPHwAYy2ntXjt2cJlAZchgNay51fbGtf_3J8AHB8tGX9aXQVsQ04V0IL8z7CoM2FBkvOSR2X&_nc_zt=23&_nc_ht=scontent.fdad1-3.fna&_nc_gid=AGKLHF-ht0pEXd_aMWS0I00&oh=00_AYCM5QLhrOmpozq7IvZJbjkFVIoOH2ycUIXj4SP5_dONzw&oe=67C5CA97'),
+('https://scontent.fdad1-1.fna.fbcdn.net/v/t1.6435-9/84868735_873609406394116_1173259587797647360_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=pQxxhaAvU40Q7kNvgFceWk5&_nc_oc=Adg7Z6tPkqmQuht6VhmHOwUBmDPDvabEgGdkUSRzcwuZoBFoyu_isX3TL9Y9H399i1GzZYFxfb1FawmZ0-DyUjYH&_nc_zt=23&_nc_ht=scontent.fdad1-1.fna&_nc_gid=AW4wJ88tPBe4mcPK4mKHusL&oh=00_AYA4OefE5ySMT_3PwE-7TP4Z8Z4x8dNdzlMTsugjx8uMMA&oe=67C5A8B1'),
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/472646206_2209191962835847_4965366483047585357_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=aZIc8hh9HoYQ7kNvgG6Seo7&_nc_oc=AdhF8pmacA99JnOltxswCPcWqrYUvM3RCy5kr11Ovezh3Jc-XaFs_PjCJBLltmgbK1-mIaCIIjknuKoJi4qPMnie&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=Azlzocurcrm09fcKOQGmlDu&oh=00_AYCDNq5iwJFEYgwNe-2RRxOqWol4JiGk41KLJhKIFtce4A&oe=67A3F824'),
+('https://scontent.fdad1-4.fna.fbcdn.net/v/t39.30808-6/453072757_472865418817106_3956231085840428114_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=0NTxhlIz0c8Q7kNvgFfHL-M&_nc_oc=AdgSWs7kZk8aQ1aXQUkLQ-pHqocxZMcPSlAtD7j2yvnceq2bqsYodFpgoOGZyl8Ly9Dh3DiUX8NXRYTJWe0zqNzj&_nc_zt=23&_nc_ht=scontent.fdad1-4.fna&_nc_gid=AljFvMscaABNCNLlyFrdUaR&oh=00_AYA2sGNXOJAbka-gXTMwiq5XMHElqNUHw0GrqT3e7ZeLcw&oe=67A422C3'),
+('https://i.pinimg.com/1200x/37/ac/12/37ac12fc37988ef93d249d629cd1f0b0.jpg'),
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/472780601_2209191839502526_6627120700551922012_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=35FnuiYhqEsQ7kNvgFNkYdy&_nc_oc=AdjJtcm3ngCUYTNvZKyh-RRsJufm5sComqcGEatlRv2ugwxhBxOFxKxu-Lzrs_Bv4OXlBWF8JHbF05lmrk2ueyHQ&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=AmKolbzlhNzg6aBAj83bGpZ&oh=00_AYBaSa4V6-Atk_EHfDpSaU1eGYCWOY8MsERj0FSAIAvfxw&oe=67A5114B'),
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/472847358_2209192199502490_2703908139889798010_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Iggvb3MfNbMQ7kNvgHNJDAG&_nc_oc=Adhhlzxmu6Z-zTppVJu3OO-mXYAL_RKAZIwLZSpbVeYU4bqWSZr-QZEulr1Hnizgv_3AE1KL90tnBEtvgZ_WkSgK&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=AseOETteBx_cAw6Lk8SXw4h&oh=00_AYA6glXimaVpd9XIzBDnO7BQsn5nhbeSeFg_ryZazAJutw&oe=67A52C42'),
+('https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/472914358_2209191812835862_6741408555249229917_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=PvkKgtowgQQQ7kNvgHgGMWZ&_nc_oc=Adj99gDtUukC9e2w64-mkU7hm2vMkMkp3k3a7HZ3hASsGuFzgRYmUE5YFPJkdVNM2gQd6TvARbhwyzR878MJeQCj&_nc_zt=23&_nc_ht=scontent.fdad1-3.fna&_nc_gid=ANkwFDNlugwfv9Ictkg2S6n&oh=00_AYAcLQGOgVNBpw4R9vl5u6bItv8oYRjdAfmov4zUNzot7w&oe=67A52774'),
+('https://scontent.fdad2-1.fna.fbcdn.net/v/t39.30808-6/472789579_2209191946169182_2530644637986047715_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=nnReLsePCu4Q7kNvgEIPzS1&_nc_oc=Adjuv23YGwOlwr_7vVxbQ23Zwb8949PP9sQLuTdxClKCbIUs7G5O8-uHeHRPabHzeTaROHiE83BEn43eghUqnRkt&_nc_zt=23&_nc_ht=scontent.fdad2-1.fna&_nc_gid=ATcoPOsn4F_WzzfmUj68J_w&oh=00_AYAGwfj-gxS81iqYRirEPcdZPWPyk7ymLv_dm-s8qZ_kFw&oe=67A51511'),
+('https://haycafe.vn/wp-content/uploads/2021/12/mau-anh-the-dep-nam-ngoi-sao-kpop-mac-ao-den.jpg'),
+('https://i.pinimg.com/736x/0d/18/b7/0d18b7af57d7fdf390edafb21403cfa4.jpg'),
+('https://i.pinimg.com/236x/c2/10/8c/c2108cbac2a933ab4e13f91d6ae76dbe.jpg'),
+('https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/84181481_873609463060777_3072694147010789376_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=qS1mEeWWYOoQ7kNvgFZ1UB_&_nc_oc=AdgMBl0G4g74fUJq9OyRrM1TTbBcO6hn4Ztv906wXCB74J060Y5gaYEMQmxTqagDVLP143EadOUoP5aIR53z3Y6i&_nc_zt=23&_nc_ht=scontent.fdad1-2.fna&_nc_gid=AQgToMQrkjovrRU7F96syt-&oh=00_AYD3jmLxmdxXnjZMh0FH5A36GFWD3x6VmpjCPOse0psK6A&oe=67C5AF4B'),
+('https://i.pinimg.com/736x/bb/d0/cc/bbd0cc7a783d12e4ef3d46f062084400.jpg');
+
+INSERT INTO user (address, dob, email, gender, name, password, phone_number, avatar_id)
+VALUES
+    -- 5 nam giảng viên
+    ('Hà Nội', '1995-02-01', 'zassuo@gmail.com', 'MALE', 'Lê Văn Việt', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987651001', 1),
+    ('Đà Nẵng', '1996-04-15', 'hung@gmail.com', 'MALE', 'Trần Mạnh Hùng', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987651002', 2),
+    ('TP HCM', '1997-06-22', 'an@gmail.com', 'MALE', 'Lê Văn An', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987651003', 3),
+    ('Hà Nội', '1998-09-13', 'thang@gmail.com', 'MALE', 'Vũ Minh Thắng', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987651004', 4),
+    ('Đà Nẵng', '1999-12-05', 'cong@gmail.com', 'MALE', 'Nguyễn Thanh Công', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987651005', 5),
+	-- 5 nữ giảng viên
+    ('TP HCM', '1995-01-12', 'mai@gmail.com', 'FEMALE', 'Võ Như Nguyệt', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652001', 6),
+    ('Hà Nội', '1996-03-17', 'tuyet@gmail.com', 'FEMALE', 'Phan Minh Tuyết Tâm', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652002', 7),
+    ('Đà Nẵng', '1997-05-25', 'lan@gmail.com', 'FEMALE', 'Bùi Võ Tú Linh', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652003', 8),
+    ('TP HCM', '1998-08-29', 'phuong@gmail.com', 'FEMALE', 'Lê Di Băng', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652004', 9),
+    ('Hà Nội', '1999-11-02', 'kimlan@gmail.com', 'FEMALE', 'Trần Tâm Nương', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652005', 10),
+    -- 10 nữ sv
+    ('Đà Nẵng', '2000-01-11', 'thuha@gmail.com', 'FEMALE', 'Nguyễn Thị Thu Hà', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652006', 11),
+    ('TP HCM', '2001-02-23', 'lananh@gmail.com', 'FEMALE', 'Vũ Thị Lan Anh', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652007', 12),
+    ('Hà Nội', '2002-03-17', 'kimngan@gmail.com', 'FEMALE', 'Bùi Thị Kim Ngân', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652008', 13),
+    ('Đà Nẵng', '2003-05-06', 'lanphuong@gmail.com', 'FEMALE', 'Trần Thị Lan Phương', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652009', 14),
+    ('TP HCM', '2004-07-25', 'doha@gmail.com', 'FEMALE', 'Nguyễn Thị Đỗ Hà', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652010', 15),
+    ('Hà Nội', '2000-01-17', 'kimanh@gmail.com', 'FEMALE', 'Lê Kim Anh', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652011', 16),
+    ('Đà Nẵng', '2001-03-22', 'ngoclan@gmail.com', 'FEMALE', 'Phan Ngọc Lan', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652012', 17),
+    ('TP HCM', '2002-05-13', 'nhuquynh@gmail.com', 'FEMALE', 'Nguyễn Thị Như Quỳnh', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652013', 18),
+    ('Hà Nội', '2003-06-26', 'minhhong@gmail.com', 'FEMALE', 'Trần Minh Hồng', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652014', 19),
+    ('Hà Nội', '2003-06-26', 'nganha@gmail.com', 'FEMALE', 'Đào Hà Ngân', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652014', 20),
+    -- 10 nam sv
+    ('Hà Nội', '2000-03-08', 'yaxsuo@gmail.com', 'MALE', 'Lê Quang Việt', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653002', 21),
+    ('Đà Nẵng', '2001-04-29', 'son@gmail.com', 'MALE', 'Trần Hồng Sơn', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653003', 22),
+    ('TP HCM', '2002-06-14', 'dat@gmail.com', 'MALE', 'Nguyễn Phát Đạt', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653004', 23),
+    ('Hà Nội', '2003-07-21', 'tuan@gmail.com', 'MALE', 'Vũ Anh Tuấn', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653005', 24),
+    ('Đà Nẵng', '2000-02-12', 'dung@gmail.com', 'MALE', 'Bùi Tiến Dũng', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653006', 25),
+    ('TP HCM', '2001-05-17', 'vanhai@gmail.com', 'MALE', 'Nguyễn Văn Hải', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653007', 26),
+    ('Hà Nội', '2002-08-23', 'ngochai@gmail.com', 'MALE', 'Phan Ngọc Hải', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653008', 27),
+    ('Đà Nẵng', '2003-10-10', 'minhnhat@gmail.com', 'MALE', 'Lê Minh Nhật', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653009', 28),
+    ('TP HCM', '2004-11-30', 'cuong@gmail.com', 'MALE', 'Trần Tiến Cường', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653010', 29),
+	('TP HCM', '1999-02-17', 'quanghuy@gmail.com', 'MALE', 'Nguyễn Quang Huy', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653001', 30),
+    -- thêm 20 sv random ko avatar
+    ('Hà Nội', '2000-02-22', 'thuy@gmail.com', 'FEMALE', 'Nguyễn Thị Thùy Dương', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652021', NULL),
+    ('Đà Nẵng', '2001-03-04', 'anhtu@gmail.com', 'FEMALE', 'Lê Thị An Hương', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652022', NULL),
+    ('TP HCM', '2002-07-10', 'huyen@gmail.com', 'FEMALE', 'Vũ Thị Huyền', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652023', NULL),
+    ('Hà Nội', '2003-09-06', 'thao@gmail.com', 'FEMALE', 'Nguyễn Thị Thảo', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652024', NULL),
+    ('Đà Nẵng', '2004-05-22', 'bichngoc@gmail.com', 'FEMALE', 'Trần Thị Bích Ngọc', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652025', NULL),
+    ('TP HCM', '2001-12-11', 'lanhong@gmail.com', 'FEMALE', 'Bùi Thị Lan Hồng', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652026', NULL),
+    ('Hà Nội', '2003-11-02', 'nguyenquyen@gmail.com', 'FEMALE', 'Nguyễn Quỳnh Như', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652027', NULL),
+    ('Đà Nẵng', '2000-10-17', 'ngoclanh@gmail.com', 'FEMALE', 'Lê Ngọc Lan Hương', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652028', NULL),
+    ('TP HCM', '2002-12-29', 'tuyetnhu@gmail.com', 'FEMALE', 'Phan Minh Tuyết Như', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987652029', NULL),
+    ('Hà Nội', '2000-04-09', 'hoangnam@gmail.com', 'MALE', 'Nguyễn Hoàng Nam', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653002', NULL),
+    ('Đà Nẵng', '2001-06-25', 'dantien@gmail.com', 'MALE', 'Trần Đan Tiến', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653003', NULL),
+    ('TP HCM', '2002-08-14', 'khangviet@gmail.com', 'MALE', 'Bùi Khánh Việt', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653004', NULL),
+    ('Hà Nội', '2003-09-18', 'longnam@gmail.com', 'MALE', 'Phan Quốc Long', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653005', NULL),
+    ('Đà Nẵng', '2004-01-02', 'kientruong@gmail.com', 'MALE', 'Nguyễn Kiến Trường', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653006', NULL),
+    ('TP HCM', '2001-11-16', 'tuananh@gmail.com', 'MALE', 'Vũ Anh Tuấn', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653007', NULL),
+    ('Hà Nội', '2002-02-05', 'phuc@gmail.com', 'MALE', 'Nguyễn Minh Phúc', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653008', NULL),
+    ('Đà Nẵng', '2003-06-19', 'thinhpham@gmail.com', 'MALE', 'Trần Thiện Pham', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653009', NULL),
+    ('TP HCM', '2004-04-28', 'tamnguyen@gmail.com', 'MALE', 'Nguyễn Thành Tâm', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653010', NULL),
+    ('TP HCM', '2002-04-28', 'minhtinh@gmail.com', 'MALE', 'Nguyễn Minh Tinh', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653011', NULL),
+    ('TP HCM', '2001-04-28', 'phongtran@gmail.com', 'MALE', 'Nguyễn Phong Trần', '$2a$10$RqGriJnkDkaKR6wZGEEpMeWGbDOOtH.pueiwbacYSVgGR4hwhOOtO', '0987653012', NULL);
+
+
+INSERT INTO teacher (degree, faculty_id, user_id)
+VALUES
+    ('MASTER', 1, 2),('DOCTOR', 2, 3),('PROFESSOR', 3, 4),('MASTER', 4, 5),('DOCTOR', 5, 6),
+    ('PROFESSOR', 1, 7),('MASTER', 2, 8),('DOCTOR', 3, 9),('PROFESSOR', 4, 10),('MASTER', 5, 11);
+
+
+INSERT INTO student (code, clazz_id, user_id)
+VALUES
+    ('HV-001', 1, 12),('HV-002', 2, 13),('HV-003', 3, 14),('HV-004', 4, 15),('HV-005', 1, 16),
+    ('HV-006', 2, 17),('HV-007', 3, 18),('HV-008', 4, 19),('HV-009', 1, 20),('HV-010', 2, 21),
+    ('HV-011', 3, 22),('HV-012', 4, 23),('HV-013', 1, 24),('HV-014', 2, 25),('HV-015', 3, 26),
+    ('HV-016', 4, 27),('HV-017', 1, 28),('HV-018', 2, 29),('HV-019', 3, 30),('HV-020', 4, 31),
+    ('HV-021', 1, 32),('HV-022', 2, 33),('HV-023', 3, 34),('HV-024', 4, 35),('HV-025', 1, 36),
+    ('HV-026', 2, 37),('HV-027', 3, 38),('HV-028', 4, 39),('HV-029', 1, 40),('HV-030', 2, 41),
+    ('HV-031', 3, 42),('HV-032', 4, 43),('HV-033', 1, 44),('HV-034', 2, 45),('HV-035', 3, 46),
+    ('HV-036', 4, 47),('HV-037', 1, 48),('HV-038', 2, 49),('HV-039', 2, 50),('HV-040', 2, 51);
+
+
+INSERT INTO user_role (user_id, role_id) VALUES
+(2, 3),(3, 3),(4, 3),(5, 3),(6, 3),(7, 3),(8, 3),(9, 3),(10, 3),(11, 3),
+(12, 2),(13, 2),(14, 2),(15, 2),(16, 2),(17, 2),(18, 2),(19, 2),(20, 2),(21, 2),
+(22, 2),(23, 2),(24, 2),(25, 2),(26, 2),(27, 2),(28, 2),(29, 2),(30, 2),(31, 2),
+(32, 2), (33, 2), (34, 2), (35, 2), (36, 2), (37, 2), (38, 2), (39, 2), (40, 2),
+(41, 2), (42, 2), (43, 2), (44, 2), (45, 2), (46, 2), (47, 2), (48, 2), (49, 2),(50, 2),(51, 2);
+
+INSERT INTO team (name) VALUES
+('Cyber Guardians'),
+('AI Pioneers'),
+('Code Avengers'),
+('Data Wizards');
+
+
